b3ce05f22048fa79f5f61836eff168d0c7d7b65f
Dat Doan <datdv1@smartosc.com>
2024-11-11 12:12:06 +0700

    Init MSBAccountsJourney

---
+++ MSBAccountsJourneyExample/MSBAccountsJourneyExample/MSBAccountsJourneyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSBAccountsJourneyExampleApp.swift
+//  MSBAccountsJourneyExample
+//
+//  Created by doandat on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MSBAccountsJourneyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MSBAccountsJourneyExample/MSBAccountsJourneyExampleTests/MSBAccountsJourneyExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  MSBAccountsJourneyExampleTests.swift
+//  MSBAccountsJourneyExampleTests
+//
+//  Created by doandat on 11/11/24.
+//
+
+import Testing
+
+struct MSBAccountsJourneyExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
