a73003e3ec8bf714c3b76068f8e4c6eeb554638d
Dat Doan <datdv1@smartosc.com>
2024-12-04 16:17:04 +0700

    Update resolver register

--- Sources/MSBAccountsJourney/Configuration/Configuration.swift
+++ Sources/MSBAccountsJourney/Configuration/Configuration.swift
@@ -6,16 +6,45 @@
 //
 
 import Foundation
-//import Resolver
+import Resolver
+import MSBUtilities
+import Moya
 
 extension MSBAccountsJourney {
     public struct Configuration {
-        /// Create a new Configuration object with default values
-        public init() {
-            // no code required
+        
+        public init() {}
+        
+        @MainActor
+        public func register() {
+            let arrangementManagerService: ArrangementManagerService = {
+                guard let config = Resolver.optional(MSBBackbaseConfiguration.self),
+                      let authPlugin = Resolver.optional(AccessTokenPlugin.self) else {
+                    fatalError("Backbase Config not found")
+                }
+                let moyaProvider: MoyaProvider<ArrangementManagerService.Target> = {
+                    return .init(
+                        session: .init(configuration: config.securitySessionConfiguration),
+                        plugins: [authPlugin]
+                    )
+                }()
+                
+
+                return ArrangementManagerService(
+                    baseURL: config.baseURL,
+                    moyaProvider: moyaProvider
+                )
+            }()
+
+            Resolver.register {
+    //            AccountsListSummaryUseCaseImp(
+    //                repository: AccountRepository(remoteDataSource: FakeArrangementManagerService())
+    //            ) as AccountsListUseCase
+                AccountsListSummaryUseCaseImp(
+                    repository: AccountRepository(remoteDataSource: arrangementManagerService)
+                ) as AccountsListUseCase
+            }
         }
-        
-        
     }
 }
 
